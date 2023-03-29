2dc662afe6475d41c04c653ab295873c604e9504
Levan <papaichik@yahoo.com>
2023-03-29 19:09:24 -0400

    Last commit

--- src/test/java/com/library/runner/CukesRunner.java
+++ src/test/java/com/library/runner/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features" ,
         glue = "com/library/steps",
         dryRun = false,
-        tags = "@Lev or @db"
+        tags ="@lev"
 )
 public class CukesRunner {
 
